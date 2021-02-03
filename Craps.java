85d94b3cefff61815c075485a15eeb3e6c60fa4c
Eddie Konjoyan <ekonjoyan22@students.polytechnic.org>
2021-02-02 21:55:31 -0800

    Finished no j-docs

--- Craps.java
+++ Craps.java
@@ -29,8 +29,10 @@ public class Craps
         if (playGame.substring(0,1).equalsIgnoreCase("y"))
         { 
             System.out.println("1. A player rolls two six-sided dice and adds the numbers rolled together.");
-            System.out.println("2. On this first roll, a 7 or an 11 automatically wins, and a 2, 3, or 12 automatically loses,and play isover. If a 4, 5, 6, 8, 9, or 10 are rolled on first roll, that number becomes the 'point.' ");
-            System.out.println("3. The player continues to roll the two diceagain until one of two things happens: either they roll the 'point' from that first roll again, in which case they win; orthey roll a 7, in which case they lose");
+            System.out.println("2. On this first roll, a 7 or an 11 automatically wins, and a 2, 3, or 12 automatically loses,and play is over.");
+            System.out.println("   If a 4, 5, 6, 8, 9, or 10 are rolled on first roll, that number becomes the 'point.' ");
+            System.out.println("3. The player continues to roll the two dice again until one of two things happens: either they roll the 'point' from that first roll again,");
+            System.out.println("   in which case they win; or they roll a 7, in which case they lose");
         }
     }
    public static int pointOne() {
@@ -64,39 +66,37 @@ public class Craps
        System.out.println("Your second total is " + point2);
        return point2;
     }
-   public static void calc(int point1, int point2, boolean exit) {
+   public static boolean calc(int point1, int point2) {
        if ((point1 == 7 || point1 ==11) && point2 == 0 ) {
            System.out.println("YOU WIN!");
-           exit = true;
+           return true;
         }
        else if ((point1 == 2 || point1 == 3 || point1 == 12) && point2 == 0) {
            System.out.println("YOU LOSE.");
-           exit = true;
+           return true;
         }
        else if (point1 == point2) {
            System.out.println("YOU WIN!");
-           exit = true;
+           return true;
         }
        else if (point2 == 7) {
            System.out.println("YOU LOSE.");
-           exit = true;
+           return true;
         }
+        return false;
    }
     
    public static void main(String[] args) {
         boolean exit = false;
-        playGame();
         while(playGame() == true) {
         instructions();
-        pointOne();
         int point1 = pointOne();
-        calc(point1, 0, exit);
-        while (exit ==false) {
+        exit = calc(point1, 0);
+        while (exit == false) {
          int point2 = pointTwo();
-         calc(point1, point2 , exit);
+         exit = calc(point1, point2);
         }
-        playGame();
     }
-    }
-
+    System.out.println("Thanks for playing!");
+   }
 }
